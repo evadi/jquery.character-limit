842fa6e2c5138ee1ac43c6b02e302c501045865d
evadi <evadi83@gmail.com>
2014-04-07 01:28:37 +0100

    Initial commit of existing files

--- README.md
+++ README.md
@@ -1,4 +1,37 @@
-jquery.character-limit
-======================
-
+# jquery.character-limit
 A very simple jQuery plugin which allows character limits to be placed on textareas
+
+## Prerequisites
+1. jQuery (tested with v1.3+)
+
+## How to use
+1. Include the plugin in your html page `<script type="text/javascript" src="jquery.character-limit.js"></script>
+2. After document ready initialise the plugin on your textarea `$("textarea").characterLimit()`
+
+## Examples
+To restrict a textarea to a maximum of 100 characters then you can use the default setting as follows
+```html
+<html>
+    <head>
+        <script type="text/javascript" src="jquery-character-limit.js" ></script>
+        <script type="text/javascript" src="main.js" ></script>
+    </head>
+    <body>
+        <textarea id="restrict"></textarea>
+    </body>
+</html>
+```
+```javascript
+$(function() {
+    $("#restrict").characterLimit();
+}
+```
+
+To customise the maximum characters allowed you can override the default settings as follow
+```javascript
+$(function() {
+    $("#restrict").characterLimit({
+        "maxCharacters":20
+    });
+}
+```
---
+++ sandbox/v0-1/jquery-character-limit-0.1.js
@@ -0,0 +1,47 @@
+//==================================================================
+// Plugin written by David Ferguson
+// http://code.davidferguson.me.uk
+//
+// version: 0.1.0
+// date: 22/03/2013
+//
+// Anyone can use/modify/copy the source on any system, for any use,
+// without express permission from the author
+//==================================================================
+
+(function ($) {
+    $.fn.characterLimit = function (options) {
+
+        var settings = $.extend({
+            'maxCharacters': 100
+        }, options);
+
+        return this.each(function () {
+
+            var $this = $(this);
+
+            $this.keypress(function (e) {
+                var char = String.fromCharCode(e.which);
+                var curVal = $(this).val();
+                var forecastVal = curVal + char;
+                
+                //when the character limit is about to be exceeded preventDefault
+                //halts the new character being assigned to the text area
+                if (forecastVal.length >= settings.maxCharacters) {
+                    e.preventDefault();
+                }
+            });
+
+            $this.bind("paste", function (e) {
+                //allows the paste operation to finish so the text is ready to inspect
+                var source = $(this);
+                setTimeout(function () {
+                    var val = source.val();
+                    source.val(val.substring(0, settings.maxCharacters));
+                }, 0);
+            });
+
+        });
+
+    };
+})(jQuery);
